174ae1e26fe403fa21682469a02a9c2c033a2c7d
Usman Malik <usman@particult.com>
2013-10-31 16:11:49 +0000

    Move from pelican to complexity

--- fabfile.py
+++ fabfile.py
@@ -3,60 +3,29 @@ import fabric.contrib.project as project
 import os
 
 
-# Local path configuration (can be absolute or relative to fabfile)
-env.content_path = 'content'
-env.local_config = 'pelicanconf.py'
-env.publish_config = 'publishconf.py'
-env.deploy_path = 'output'
+env.content_path = 'src'
+env.deploy_path = 'www'
 DEPLOY_PATH = env.deploy_path
 
 # Remote server configuration
 production = 'wf'
 dest_path = '~/webapps/asterias'
 
-# Rackspace Cloud Files configuration settings
-env.cloudfiles_username = 'my_rackspace_username'
-env.cloudfiles_api_key = 'my_rackspace_api_key'
-env.cloudfiles_container = 'my_cloudfiles_container'
-
-
 def clean():
     if os.path.isdir(DEPLOY_PATH):
         local('rm -rf {deploy_path}'.format(**env))
-        local('mkdir {deploy_path}'.format(**env))
 
 def build():
-    local('pelican {content_path} -o {deploy_path} -s {local_config}'.format(**env))
-
-def rebuild():
     clean()
-    build()
-
-def regenerate():
-    local('pelican -r -s pelicanconf.py')
+    local('complexity --noserver {content_path}'.format(**env))
 
 def serve():
-    local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
-
-def reserve():
-    build()
-    serve()
-
-def preview():
-    local('pelican {content_path} -o {deploy_path} -s {publish_config}'.format(**env))
-
-def cf_upload():
-    rebuild()
-    local('cd {deploy_path} && '
-          'swift -v -A https://auth.api.rackspacecloud.com/v1.0 '
-          '-U {cloudfiles_username} '
-          '-K {cloudfiles_api_key} '
-          'upload -c {cloudfiles_container} .'.format(**env))
+    clean()
+    local('complexity {content_path}'.format(**env))
 
 @hosts(production)
 def publish():
-    clean()
-    local('pelican {content_path} -o {deploy_path} -s {publish_config}'.format(**env))
+    build()
     project.rsync_project(
         remote_dir=dest_path,
         exclude=[".DS_Store","drafts"],
