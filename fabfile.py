05f9392516d40289df99ea7c50a7da7033fcce83
Usman Malik <usman@particult.com>
2013-11-08 01:56:20 +0000

    Clobbered files, so new start

--- fabfile.py
+++ fabfile.py
@@ -2,33 +2,45 @@ from fabric.api import *
 import fabric.contrib.project as project
 import os
 
-
-env.content_path = 'src'
-env.deploy_path = 'www'
+# Local path configuration (can be absolute or relative to fabfile)
+env.deploy_path = 'output'
 DEPLOY_PATH = env.deploy_path
 
 # Remote server configuration
 production = 'wf'
-dest_path = '~/webapps/asterias'
+dest_path = 'path/to/out'
 
 def clean():
     if os.path.isdir(DEPLOY_PATH):
         local('rm -rf {deploy_path}'.format(**env))
+        local('mkdir {deploy_path}'.format(**env))
 
 def build():
+    local('pelican -s pelicanconf.py')
+
+def rebuild():
     clean()
-    local('complexity --noserver {content_path}'.format(**env))
+    build()
+
+def regenerate():
+    local('pelican -r -s pelicanconf.py')
 
 def serve():
-    clean()
-    local('complexity {content_path}'.format(**env))
+    local('cd {deploy_path} && python -m SimpleHTTPServer'.format(**env))
+
+def reserve():
+    build()
+    serve()
+
+def preview():
+    local('pelican -s publishconf.py')
 
 @hosts(production)
 def publish():
-    build()
+    local('pelican -s publishconf.py')
     project.rsync_project(
         remote_dir=dest_path,
-        exclude=[".DS_Store","drafts"],
+        exclude=['.DS_Store', 'drafts'],
         local_dir=DEPLOY_PATH.rstrip('/') + '/',
         delete=True
     )
